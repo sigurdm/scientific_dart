1ad2cd49888863a567211c699077f29ce0370ca6
Sigurd Meldgaard <sigurdm@google.com>
2026-06-19 09:05:12 +0000

    Add missing 3D Stack Matrix Determinant test from final synthesis (Phase 3)
    TAG=agy
    CONV=55f57610-629a-4a4b-bbc1-7d50e4c10103

--- pkgs/ndarray/test/math/linear_algebra_test.dart
+++ pkgs/ndarray/test/math/linear_algebra_test.dart
@@ -1796,6 +1796,27 @@ void main() {
         expect(dList[1].imag, closeTo(0.0, 1e-4));
       }),
     );
+
+    test(
+      '3D Stack Matrix Determinant',
+      () => NDArray.scope(() {
+        final a = NDArray.fromList(
+          Float64List.fromList([
+            1.0, 2.0, 3.0, 4.0, // Matrix 0: det = -2
+            5.0, 6.0, 7.0, 8.0, // Matrix 1: det = -2
+          ]),
+          [2, 2, 2],
+          DType.float64,
+        );
+
+        final d = det(a);
+
+        expect(d.shape, [2]);
+        final dList = d.toList();
+        expect(dList[0], closeTo(-2.0, 1e-9));
+        expect(dList[1], closeTo(-2.0, 1e-9));
+      }),
+    );
   });
 
   group('Complex Matrix Multiplication Tests', () {
