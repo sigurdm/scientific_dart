6ed6b4ef8824d84af72947cde73c08a8d8119830
Sigurd Meldgaard <sigurdm@google.com>
2026-06-23 09:05:16 +0000

    fix(build): remove tracked hwy_build files from git and ignore hwy_build/
    - Removed accidentally committed local build cache hwy_build/ from git tracking.
    - Updated pkgs/ndarray/.gitignore to ignore third_party/highway/hwy_build/.
    - Added a verification step Verify MSVC Environment in .github/workflows/dart.yml to debug Windows MSVC env issues.
    
    TAG=agy
    CONV=7eee3586-f7b9-4bc2-8bd6-ddd5b9154a50

--- pkgs/ndarray/third_party/highway/hwy_build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/google/home/sigurdm/projects/math/pkgs/ndarray/third_party/highway
-# Build directory: /usr/local/google/home/sigurdm/projects/math/pkgs/ndarray/third_party/highway/hwy_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
